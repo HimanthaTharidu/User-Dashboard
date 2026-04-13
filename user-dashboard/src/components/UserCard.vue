1d5e128ca3ea994da1368c0151452531d2e3fd0f
HimanthaTharidu <eg255691@engug.ruh.ac.lk>
2026-04-13 20:00:33 +0530

    create a user card component to display user

---
+++ user-dashboard/src/components/UserCard.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import type { User } from '@/types/user';
+
+defineProps<{
+    user: User
+}>()
+</script>
+
+<template>
+  <div>
+    <img :src="user.image" alt="user image">
+    <h2>{{ user.firstname }} {{ user.lastname }}</h2>
+    <p>{{ user.email }}</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
