5710e76cae775ac12dd1407cd4796d82447e9318
HimanthaTharidu <eg255691@engug.ruh.ac.lk>
2026-04-13 22:47:41 +0530

    create main dashboard for display details

---
+++ user-dashboard/src/views/Dashboard.vue
@@ -0,0 +1,49 @@
+<script setup lang="ts">
+import {ref, onMounted, computed} from 'vue'
+import { fetchUsers } from '@/services/api';
+import type { User } from '@/types/user';
+import UserCard from '@/components/UserCard.vue';
+import UserModel from '@/components/UserModel.vue';
+
+const search = ref("")
+const users = ref<User[]>([])
+console.log(users)
+
+onMounted(async () => {
+    const data = await fetchUsers()
+    users.value = data.users
+})
+
+const filterdUsers = computed(() => {
+  return users.value.filter(u => 
+  u.firstName.toLowerCase().includes(search.value.toLowerCase()))
+})
+
+const selectedUser = ref<User | null>(null)
+</script>
+
+<template>
+    <div>
+        <h1>User Dashboard</h1>
+        <div>
+            <input
+            v-model="search"
+            placeholder="Search users...">
+
+            <div>
+                <UserCard
+                v-for="user in filterdUsers" 
+                :key="user.id"
+                :user="user"
+                @select="selectedUser = $event"
+                />
+                <UserModel
+                :user="selectedUser"
+                @close="selectedUser = null"
+                />
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+</style>
