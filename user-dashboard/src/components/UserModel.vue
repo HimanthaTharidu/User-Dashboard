ba93560425e2dc9187c28435506d03853e6d8a16
HimanthaTharidu <eg255691@engug.ruh.ac.lk>
2026-04-13 22:45:38 +0530

    create modal component for dislay user details

---
+++ user-dashboard/src/components/UserModel.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import type { User } from '@/types/user';
+
+defineProps<{
+ user: User | null
+}>()
+
+const emit = defineEmits(['close'])
+</script>
+
+<template>
+    <div v-if="user">
+        <p>{{ user.email }}</p>
+        <p>{{ user.phone }}</p>
+        <button @click="emit('close')">close</button>
+    </div>
+</template>
